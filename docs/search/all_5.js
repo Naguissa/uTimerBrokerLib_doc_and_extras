c184472d229fa5b93429ddd5492654a16a136015
Naguissa <naguissa@foroelectro.net>
2024-06-20 08:38:37 +0200

    Updated to 1.0.2

--- docs/search/all_5.js
+++ docs/search/all_5.js
@@ -2,5 +2,6 @@ var searchData=
 [
   ['utimerbrokerlib_0',['uTimerBrokerLib',['../classuTimerBrokerLib.html',1,'']]],
   ['utimerbrokerlib_2eh_1',['uTimerBrokerLib.h',['../uTimerBrokerLib_8h.html',1,'']]],
-  ['utimerbrokerlib_5fmaxhooks_2',['UTIMERBROKERLIB_MAXHOOKS',['../uTimerBrokerLib_8h.html#a5d72c92b275ed22de7002818253ca5be',1,'uTimerBrokerLib.h']]]
+  ['utimerbrokerlib_5ferror_2',['UTIMERBROKERLIB_ERROR',['../uTimerBrokerLib_8h.html#a61a7488d8f278b9dab1fa9dd02d02543',1,'uTimerBrokerLib.h']]],
+  ['utimerbrokerlib_5fmaxhooks_3',['UTIMERBROKERLIB_MAXHOOKS',['../uTimerBrokerLib_8h.html#a5d72c92b275ed22de7002818253ca5be',1,'uTimerBrokerLib.h']]]
 ];
--- docs/search/defines_1.js
+++ docs/search/defines_1.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['utimerbrokerlib_5fmaxhooks_0',['UTIMERBROKERLIB_MAXHOOKS',['../uTimerBrokerLib_8h.html#a5d72c92b275ed22de7002818253ca5be',1,'uTimerBrokerLib.h']]]
+  ['utimerbrokerlib_5ferror_0',['UTIMERBROKERLIB_ERROR',['../uTimerBrokerLib_8h.html#a61a7488d8f278b9dab1fa9dd02d02543',1,'uTimerBrokerLib.h']]],
+  ['utimerbrokerlib_5fmaxhooks_1',['UTIMERBROKERLIB_MAXHOOKS',['../uTimerBrokerLib_8h.html#a5d72c92b275ed22de7002818253ca5be',1,'uTimerBrokerLib.h']]]
 ];
